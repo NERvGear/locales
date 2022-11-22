2cddd429a0380f6c490772965b77a22e0555c3ee
Lawliet0 <59295070+Lawliet0@users.noreply.github.com>
2022-11-22 22:19:22 +0300

    Add files via upload

--- tr/extensions/com.gpbeta.widget.nixie-clock.ts
+++ tr/extensions/com.gpbeta.widget.nixie-clock.ts
@@ -1,27 +1,27 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="tr" sourcelanguage="en">
 <context>
     <name>NixieWidget</name>
     <message>
         <location filename="../../../bin/Packages/com.gpbeta.widget.nixie-clock/qml/NixieWidget.qml" line="11"/>
         <source>NIXIE Tube Clock</source>
-        <translation type="unfinished"></translation>
+        <translation>Nixie tüplü saat</translation>
     </message>
     <message>
         <location filename="../../../bin/Packages/com.gpbeta.widget.nixie-clock/qml/NixieWidget.qml" line="23"/>
         <source>Time</source>
-        <translation type="unfinished"></translation>
+        <translation>Zaman</translation>
     </message>
     <message>
         <location filename="../../../bin/Packages/com.gpbeta.widget.nixie-clock/qml/NixieWidget.qml" line="30"/>
         <source>Date</source>
-        <translation type="unfinished"></translation>
+        <translation>Tarih</translation>
     </message>
     <message>
         <location filename="../../../bin/Packages/com.gpbeta.widget.nixie-clock/qml/NixieWidget.qml" line="37"/>
         <source>Divergence Meter</source>
-        <translation type="unfinished"></translation>
+        <translation>Sapma Ölçer</translation>
     </message>
 </context>
 </TS>
